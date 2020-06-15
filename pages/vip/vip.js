e95e6205414c26f551233337f7ade009fe1a0fa9
21462X <2274457767@qq.com>
2020-06-15 08:35:55 +0800

    6.12提交记录

--- pages/vip/vip.js
+++ pages/vip/vip.js
@@ -5,88 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
-    navData: ['会员推荐', '读书会', '杂志', '盐选专栏', 'Live讲座'],
-    currentTab: 0,
-    navScrollLeft: 0
+    winHeight:"",//窗口高度
+    currentTab:0, //预设当前项的值
+    scrollLeft:0, //tab标题的滚动条位置
   },
 
-  switchNav(event){
-    var cur = event.currentTarget.dataset.current; 
-    //每个tab选项宽度占1/5
-    var singleNavWidth = this.data.windowWidth / 5;
-    //tab选项居中                            
-    this.setData({
-        navScrollLeft: (cur - 2) * singleNavWidth
-    })      
-    if (this.data.currentTab == cur) {
-        return false;
-    } else {
+    // 滚动切换标签样式
+    switchTab:function(e){
         this.setData({
-            currentTab: cur
+            currentTab:e.detail.current
+        });
+        this.checkCor();
+    },
+    // 点击标题切换当前页时改变样式
+    swichNav:function(e){
+        var cur=e.target.dataset.current;
+        if(this.data.currentTaB==cur){return false;}
+        else{
+            this.setData({
+                currentTab:cur
+            })
+        }
+    },
+    //判断当前滚动超过一屏时，设置tab标题滚动条。
+    checkCor:function(){
+      if (this.data.currentTab>4){
+        this.setData({
+          scrollLeft:300
         })
-    }
-},
-switchTab(event){
-    var cur = event.detail.current;
-    var singleNavWidth = this.data.windowWidth / 5;
-    this.setData({
-        currentTab: cur,
-        navScrollLeft: (cur - 2) * singleNavWidth
-    });
-},
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+      }else{
+        this.setData({
+          scrollLeft:0
+        })
+      }
+    },
+    onLoad: function() {  
+        var that = this; 
+        //  高度自适应
+        wx.getSystemInfo( {  
+            success: function( res ) {  
+                var clientHeight=res.windowHeight,
+                    clientWidth=res.windowWidth,
+                    rpxR=750/clientWidth;
+              var  calc=clientHeight*rpxR-180;
+                console.log(calc)
+                that.setData( {  
+                    winHeight: calc  
+                });  
+            }  
+        });
+    },  
 })
